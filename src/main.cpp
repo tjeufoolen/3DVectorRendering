a6218232fd546fb6e1d193ac5a23900db864f7c3
Tjeu Foolen <12643433+tjeufoolen@users.noreply.github.com>
2020-12-09 12:57:55 +0100

    Added cool ascii art

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,19 @@
 #include "utils/logger.h"
 #include "app.h"
 
+//
+//  ,--.   ,--.,--.  ,--.  ,---.  ,--.
+//  |  |   |  ||  ,'.|  | /  O  \ |  |
+//  |  |   |  ||  |' '  ||  .-.  ||  |
+//  |  '--.|  ||  | `   ||  | |  ||  '--.
+//  `-----'`--'`--'  `--'`--' `--'`-----'
+//
+// This code is written for the linear algebra course which is part of
+// the software architecture minor.
+//
+// Authors: Stan Geitel & Tjeu Foolen
+//
+
 int main(int argc, char *argv[])  {
 
     utils::logger::getInstance().toggleDebugMessages(true);
